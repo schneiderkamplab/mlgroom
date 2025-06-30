30ac7d5049c5c131ca8e5bf7c39e78833f45fc00
Peter Schneider-Kamp <petersk@imada.sdu.dk>
2025-06-30 15:19:17 +0200

    fix

--- mlgroom/slurm.py
+++ mlgroom/slurm.py
@@ -301,7 +301,10 @@ def resubmit(job, queue_file, log_file, yes):
 
         # Update job struct
         j["job_ids"] = updated_job_ids
-        j["submitted"] = format_ranges(split_into_ranges(sorted(updated_submitted)))
+        j["submitted"] = [
+            f"{s}-{e}" if s != e else str(s)
+            for (s, e) in split_into_ranges(sorted(updated_submitted))
+        ]
         j["resubmit_counts"] = resubmit_counts
         j["failed"] = []
         modified = True
